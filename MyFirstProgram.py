a994cbe7333ed8f7126c4dfd0bc550b6a615edd3
nyambane12 <42916522+nyambane12@users.noreply.github.com>
2018-09-02 23:27:11 +0300

    Create MyFirstProgram.py
    program to ask the user to enter name and email adress

---
+++ MyFirstProgram.py
@@ -0,0 +1,11 @@
+class finder:
+ def name_user(self, name, email_adress):
+  name = raw_input("Please Enter your name:")
+  print ("name")
+ def email_user(self):
+  email = raw_input("Enter your email adress:")
+  print("email")
+ user == finder()
+ user.name()
+ user.email()
+  
